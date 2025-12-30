38f505c661432c45d45e70d039bb159dba609c53
Anders Jacobsson <anders.jacobsson@columbusglobal.com>
2025-12-30 12:08:30 +0100

    Dockerized

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
+    output: 'standalone',
     reactCompiler: true,
     experimental: {
         serverActions: {
