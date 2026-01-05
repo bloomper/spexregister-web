c9ecef3a4c6262d96fa7e200a316a71752256b47
Anders Jacobsson <anders.jacobsson@columbusglobal.com>
2026-01-05 16:29:55 +0100

    Corrected UTC date parsing

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,32 +1,31 @@
 import {type ClassValue, clsx} from "clsx";
 import {twMerge} from "tailwind-merge";
+import {format, parseISO} from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
 export function formatDate(date: string) {
-    const m = /^(\d{4})-(\d{2})-(\d{2})/.exec(date);
-
-    if (!m) {
+    if (!date) {
         return '';
     }
-
-    const year = m[1];
-    const month = m[2];
-    const day = m[3];
-
-    return `${year}-${month}-${day}`;
+    try {
+        return format(parseISO(date), "yyyy-MM-dd");
+    } catch (e) {
+        return '';
+    }
 }
 
 export function formatDateTime(date: string) {
-    const m = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2})/.exec(date);
-
-    if (!m) {
+    if (!date) {
+        return '';
+    }
+    try {
+        return format(parseISO(date), "yyyy-MM-dd HH:mm");
+    } catch (e) {
         return formatDate(date);
     }
-
-    return `${m[1]}-${m[2]}-${m[3]} ${m[4]}:${m[5]}`;
 }
 
 export function getProxiedImageUrl(url?: string | null, lastModifiedAt?: string | null) {
